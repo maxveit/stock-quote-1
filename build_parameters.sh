70861a63187cb17843927f0dbedad8f8d1cf93c6
Max Veit <55698339+maxveit@users.noreply.github.com>
2020-12-04 11:17:16 +0100

    Update build_parameters.sh
    switched namespace for image push

--- build_parameters.sh
+++ build_parameters.sh
@@ -4,7 +4,7 @@ maven_goals="install"
 
 image_name="stock-quote"
 image_tag="latest"
-image_namespace="stocktrader"
+image_namespace="cicd"
 
 deployment_create="true"
 deplyoment_namespace="stocktrader"
